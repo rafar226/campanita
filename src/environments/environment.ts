31540ed4355320a569a269cd36cd0f30ca5164b5
Rafael del Valle <64286217+rafar226@users.noreply.github.com>
2021-02-09 00:55:26 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseconfig: {
-    apiKey: "AIzaSyAya90nbue-yDDTS2kEg3RhRQTNBycHGFg",
-    authDomain: "campanita-salon.firebaseapp.com",
-    projectId: "campanita-salon",
-    storageBucket: "campanita-salon.appspot.com",
-    messagingSenderId: "906432448139",
-    appId: "1:906432448139:web:d4f320030383ca91a1368e",
-    measurementId: "G-61LHVHZTDN"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx",
+    measurementId: "xxx"
   }
 };
 
